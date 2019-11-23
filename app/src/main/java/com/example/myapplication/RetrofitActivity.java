d447d023d662a8c6ec5c8c1779ecba82be74d302
Zakuto <ahmadalbab1412@gmail.com>
2019-11-24 07:06:03 +0800

    Add Retrofit Module 2

---
+++ app/src/main/java/com/example/myapplication/RetrofitActivity.java
@@ -0,0 +1,60 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.app.ProgressDialog;
+import android.os.Bundle;
+
+import com.example.myapplication.adapter.CustomAdapter;
+import com.example.myapplication.endpoints.GetDataService;
+import com.example.myapplication.model.RetroPhoto;
+import com.example.myapplication.network.RetrofitClientInstance;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class RetrofitActivity extends AppCompatActivity {
+
+    private CustomAdapter customAdapter;
+    private RecyclerView recyclerView;
+    ProgressDialog progressDialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_retrofit);
+
+        progressDialog = new ProgressDialog(RetrofitActivity.this);
+        progressDialog.setMessage("Loading...");
+        progressDialog.show();
+
+        GetDataService service = RetrofitClientInstance.getRetrofitInstance().create(GetDataService.class);
+        Call<List<RetroPhoto>> call = service.getAllPhotos();
+        call.enqueue(new Callback<List<RetroPhoto>>() {
+            @Override
+            public void onResponse(Call<List<RetroPhoto>> call, Response<List<RetroPhoto>> response) {
+                progressDialog.dismiss();
+                generateDataList(response.body());
+
+            }
+
+            @Override
+            public void onFailure(Call<List<RetroPhoto>> call, Throwable t) {
+
+            }
+        });
+    }
+
+    private void generateDataList(List<RetroPhoto> photoList){
+        recyclerView = findViewById(R.id.customRecyclerView);
+        customAdapter = new CustomAdapter(this,photoList);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(RetrofitActivity.this);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(customAdapter);
+    }
+}
